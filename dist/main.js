707a58e204decba8742f30de4f64bb066fc1beeb
akmgmerakmgmer <ahmedever80@gmail.com>
2025-10-19 12:35:50 +0300

    New

--- dist/main.js
+++ dist/main.js
@@ -4,13 +4,13 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const http_exception_filter_1 = require("./common/filters/http-exception.filter");
-const cookieParser = require("cookie-parser");
+const cookie_parser_1 = require("cookie-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: {
             origin: process.env.FRONTEND_ORIGIN || true,
             credentials: true,
         } });
-    app.use(cookieParser());
+    app.use((0, cookie_parser_1.default)());
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     app.useGlobalFilters(new http_exception_filter_1.GlobalHttpExceptionFilter());
     await app.listen(process.env.PORT || 4000);
