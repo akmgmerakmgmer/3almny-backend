10c5f1514dc1a1db69971c23212aa7283eb49461
akmgmerakmgmer <ahmedever80@gmail.com>
2025-10-19 12:42:38 +0300

    Configure NestJS for Vercel serverless deployment

--- dist/main.js
+++ dist/main.js
@@ -5,15 +5,38 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 const http_exception_filter_1 = require("./common/filters/http-exception.filter");
 const cookie_parser_1 = require("cookie-parser");
-async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: {
+const platform_express_1 = require("@nestjs/platform-express");
+const express_1 = require("express");
+const expressApp = (0, express_1.default)();
+let cachedApp = null;
+async function createApp() {
+    if (cachedApp) {
+        return cachedApp;
+    }
+    const adapter = new platform_express_1.ExpressAdapter(expressApp);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, adapter, {
+        cors: {
             origin: process.env.FRONTEND_ORIGIN || true,
             credentials: true,
-        } });
+        },
+    });
     app.use((0, cookie_parser_1.default)());
     app.useGlobalPipes(new common_1.ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
     app.useGlobalFilters(new http_exception_filter_1.GlobalHttpExceptionFilter());
-    await app.listen(process.env.PORT || 4000);
+    await app.init();
+    cachedApp = app;
+    return app;
 }
-bootstrap();
+async function bootstrap() {
+    const app = await createApp();
+    await app.listen(process.env.PORT || 4000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+}
+if (process.env.NODE_ENV !== 'production') {
+    bootstrap();
+}
+exports.default = async (req, res) => {
+    await createApp();
+    return expressApp(req, res);
+};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,49 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
 import cookieParser from 'cookie-parser';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express, { Request, Response } from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: {
-    origin: process.env.FRONTEND_ORIGIN || true,
-    credentials: true,
-  }});
+// Create Express app instance for serverless
+const expressApp = express();
+let cachedApp: any = null;
+
+async function createApp() {
+  if (cachedApp) {
+    return cachedApp;
+  }
+
+  const adapter = new ExpressAdapter(expressApp);
+  const app = await NestFactory.create(AppModule, adapter, {
+    cors: {
+      origin: process.env.FRONTEND_ORIGIN || true,
+      credentials: true,
+    },
+  });
+  
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useGlobalFilters(new GlobalHttpExceptionFilter());
-  await app.listen(process.env.PORT || 4000);
-  // eslint-disable-next-line no-console
+  
+  await app.init();
+  cachedApp = app;
+  return app;
 }
-bootstrap();
+
+async function bootstrap() {
+  const app = await createApp();
+  await app.listen(process.env.PORT || 4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+// Export for Vercel serverless
+export default async (req: Request, res: Response) => {
+  await createApp();
+  return expressApp(req, res);
+};
+
